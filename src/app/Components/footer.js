8a5c04cac0fae155cd3f033404dbc8d66f1be3ae
ScottDenton <scottyd_14@hotmail.com>
2018-11-30 18:02:44 -0800

    Finished

--- src/app/Components/footer.js
+++ src/app/Components/footer.js
@@ -3,7 +3,12 @@ import React from 'react';
 export class Footer extends React.Component {
   render(){
     return (
-      <footer> Made by Scott Denton, using React JS </footer>
+      <footer> Made by Scott Denton, using React JS <br/>
+        <a href = "https://github.com/ScottDenton/React-ToDo-List"
+        target = 'blank'>
+        Click to see Github repo
+        </a>
+      </footer>
     )
   }
 }
